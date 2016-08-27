eedf55b482d4b102dbe2bfe22f328996e8616b24
lir <lir48290@gmail.com>
2016-08-27 12:49:24 +0300

    add visual keyboard

--- app/src/main/java/com/liron/crypticcrossword/MainActivity.java
+++ app/src/main/java/com/liron/crypticcrossword/MainActivity.java
@@ -1,53 +1,94 @@
 package com.liron.crypticcrossword;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.GridLayout.LayoutParams;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String DELIMITER = ",";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setBoardImage();
 
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
+        createKeyboard(R.array.hebrew);
 
         PinkButton pinkButton = new PinkButton(this);
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK) {
-            if (data == null) {
-                //Display an error
-                return;
+    private void createKeyboard(int keyboardId) {
+        GridLayout grid = (GridLayout) findViewById(R.id.keyboard);
+        String[] keyboardLines = getResources().getStringArray(keyboardId);
+        setGridDimensions(grid, keyboardLines);
+        int rowIndex = 0;
+        for (String line : keyboardLines) {
+            int columnIndex = 0;
+            String[] split = line.split(DELIMITER);
+            for (String letter : split) {
+                addKey(grid, letter, rowIndex, columnIndex);
+                columnIndex++;
             }
-            try {
-                InputStream inputStream = this.getContentResolver().openInputStream(data.getData());
-                findViewById(R.id.layout).setBackground(Drawable.createFromStream(inputStream, "img"));
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            //Now you can do whatever you want with your inpustream, save it as file, upload to a server, decode a bitmap...
+            rowIndex++;
         }
     }
 
+    private void setGridDimensions(GridLayout grid, String[] keyboardLines) {
+        grid.setRowCount(keyboardLines.length);
+        grid.setColumnCount(keyboardLines[0].split(DELIMITER).length);
+    }
+
+    private void addKey(GridLayout grid, String letter, int row, int column) {
+        TextView key = new TextView(this);
+        key.setText(letter);
+
+        LayoutParams param = new LayoutParams();
+        param.height = 0;
+        param.width = 0;
+        param.setGravity(Gravity.CENTER);
+        param.rowSpec = GridLayout.spec(row, 1f);
+        param.columnSpec = GridLayout.spec(column, 1f);
+        key.setLayoutParams(param);
+        key.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        key.setBackgroundResource(R.drawable.border);
+        grid.addView(key);
+    }
+
+    private void setBoardImage() {
+        InputStream inputStream = null;
+        try {
+            inputStream = this.getContentResolver().openInputStream((Uri) getIntent().getExtras().get("boardImageUri"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        findViewById(R.id.layout).setBackground(Drawable.createFromStream(inputStream, "img"));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -69,5 +110,45 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.liron.crypticcrossword/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.liron.crypticcrossword/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
---
+++ app/src/main/java/com/liron/crypticcrossword/WelcomeActivity.java
@@ -0,0 +1,79 @@
+package com.liron.crypticcrossword;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private Uri boardImageUri = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wellcome);
+    }
+
+    public void selectImageFromGallery(View view) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
+    }
+
+    public void captureImageByCamera(View view) {
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 3);
+            permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, 2);
+        }
+    }
+
+    public void loadImage(View view) {
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (data == null) {
+                //Display an error
+                return;
+            }
+            try {
+                boardImageUri = data.getData();
+                InputStream inputStream = this.getContentResolver().openInputStream(boardImageUri);
+                findViewById(R.id.previewImage).setBackground(Drawable.createFromStream(inputStream, "img"));
+                findViewById(R.id.applyImage).setVisibility(View.VISIBLE);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void applyImage(View view) {
+        if (boardImageUri != null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra("boardImageUri", boardImageUri);
+            startActivity(intent);
+        }
+    }
+}
