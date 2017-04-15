3078a3cb7430939a73f2602e3ba928002fde038e
lir <lir48290@gmail.com>
2017-04-15 13:53:59 +0300

    adding back button to toolbar

--- app/src/main/java/com/liron/crypticcrossword/MainActivity.java
+++ app/src/main/java/com/liron/crypticcrossword/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -94,18 +95,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem item = menu.findItem(R.id.action_shareBoard);
+
+        ShareActionProvider mShareActionProvider = new ShareActionProvider(this);
+        mShareActionProvider.setShareIntent(createShareIntent());
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private Intent createShareIntent() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, "I share text!!!");
+        intent.putExtra(Intent.EXTRA_TITLE, "the Title");
+//        startActivity(Intent.createChooser(intent, "share"));
+        return intent;
     }
 }
